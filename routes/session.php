7c4c1936f2ce6563523c0072cb6f5bed82610e88
Craig McNamara <cmcnamara87@gmail.cok>
2014-05-20 20:55:03 +1000

    Add user resource with login and logout

--- routes/session.php
+++ routes/session.php
@@ -13,42 +13,43 @@ $authenticate = function ($app) {
     };
 };
 
-/**
- * Logs in
- */
-$app->post("/login", function () use ($app) {
+$app->group('/users', function () use ($app) {
+    /**
+     * Logs in
+     */
+    $app->post("/login", function () use ($app) {
 
-	$loginData = json_decode($app->request->getBody());
+        $loginData = json_decode($app->request->getBody());
 
-    $user  = R::findOne( 'user', ' email = :email ', array(':email' => $loginData->email));
+        $user  = R::findOne( 'user', ' email = :email ', array(':email' => $loginData->email));
 
-    if($user && $user->password == hash('md5', $loginData->password)) {
-    	$_SESSION['userId'] = $user->id;
-    } else {
-    	$app->halt('400', 'Incorrect email or password.');
-    }
-    echo json_encode($user, JSON_NUMERIC_CHECK);
-});
+        if($user && $user->password == hash('md5', $loginData->password)) {
+            $_SESSION['userId'] = $user->id;
+        } else {
+            $app->halt('400', 'Incorrect email or password.');
+        }
+        echo json_encode($user, JSON_NUMERIC_CHECK);
+    });
 
 
-/**
- * Creates a new user
- */
-$app->post('/register', function() use ($app) {
+    /**
+     * Creates a new user
+     */
+    $app->post('/register', function() use ($app) {
 
-	$sampleUserData = array(
-		'firstName' 	=> 'Craig',
-		'lastName'		=> 'McNamara',
-		'email'			=> 'cmcnamara87@gmail.com',
-	);
+        $sampleUserData = array(
+            'firstName'     => 'Craig',
+            'lastName'      => 'McNamara',
+            'email'         => 'cmcnamara87@gmail.com',
+        );
 
-	$userData = json_decode($app->request->getBody());
-	// $userData = $sampleUserData;
+        $userData = json_decode($app->request->getBody());
+        // $userData = $sampleUserData;
 
-	$user = R::dispense('user');
-	$user->import($userData);
-	$user->processed = mktime(0,0,0);
-	R::store($user);
+        $user = R::dispense('user');
+        $user->import($userData);
+        R::store($user);
 
-	echo json_encode($user->export(), JSON_NUMERIC_CHECK);
+        echo json_encode($user->export(), JSON_NUMERIC_CHECK);
+    });
 });
